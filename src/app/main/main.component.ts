cc1f5292d30700c2e34a221dd608c436aa24145b
Mr.Jack <techjack117@outlook.com>
2017-04-18 17:58:33 +0800

    update with index page

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private elementRef:ElementRef) { }
 
   ngOnInit() {
+  }
+  ngAfterViewInit() {
+	  var s = document.createElement("script");
+	  s.type = "text/javascript";
+	  s.src = "assets/js/jquery.js";
+	  this.elementRef.nativeElement.appendChild(s);
+
+	  var t = document.createElement("script");
+	  t.type = "text/javascript";
+	  t.src = "assets/js/main.js";
+	  this.elementRef.nativeElement.appendChild(t);
   }
 
 }
