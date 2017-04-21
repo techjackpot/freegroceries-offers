ef9fe76d48b5caa69eae0da794eb789aa22dd89e
Mr.Jack <techjack117@outlook.com>
2017-04-21 10:17:43 +0800

    add express server instead of ng serve

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(bodyParser.json());
+
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8001, function () {
+	var port = server.address().port;
+	console.log("App now running on port", port);
+});
