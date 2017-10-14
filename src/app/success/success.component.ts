c0d91b52e3df7b15450a3c3525a256c7a1db6663
Mr.Jack <zsniper117@gmail.com>
2017-10-14 11:56:00 +0900

    update: success page

--- src/app/success/success.component.ts
+++ src/app/success/success.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-success',
@@ -7,12 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuccessComponent implements OnInit {
 
-  constructor() { }
+  constructor(private elementRef:ElementRef) { }
 
   fname : string = '';
 
   ngOnInit() {
   	this.fname = window.localStorage.getItem('fname');
+  }
+
+  ngAfterViewInit() {
+	  var s = document.createElement("script");
+	  s.type = "text/javascript";
+	  s.src = "/assets/js/jquery.js";
+	  this.elementRef.nativeElement.appendChild(s);
+
+	  var t = document.createElement("script");
+	  t.type = "text/javascript";
+	  t.src = "/assets/js/main1.js";
+	  this.elementRef.nativeElement.appendChild(t);
   }
 
 }
